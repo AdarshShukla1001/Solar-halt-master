6ea990cd79d8f947c39b9ede689718e27c941529
Abhay Sharma <abhay@techcrumb.com>
2025-11-22 21:25:20 +0530

    refactor: remove old project detail and projects page, redirect to new gallery page

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { redirect } from "next/navigation";
-
-export default function ProjectsPage() {
-  // Redirect any access to /projects to the new gallery page
-  redirect("/gallery");
-  return null;
-}
